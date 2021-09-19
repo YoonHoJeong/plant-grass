167b8932e675ba04bd69f046e568c4a437511487
JeongYunHo <uno8941@gmail.com>
2021-09-19 15:32:38 +0900

    add normailze css

--- src/appTest.jsx
+++ src/appTest.jsx
@@ -1,3 +1,4 @@
+import { style } from "dom-helpers";
 import React from "react";
 import styles from "./appTest.module.css";
 
@@ -7,59 +8,66 @@ const AppTest = (props) => (
       <li className="page">main</li>
       <li className="page">home</li>
     </ul>
-    <main>
-      <header>
-        <section>
-          <div>
-            <img src="logo512.png" alt="Profile IMG" />
-            <div>
-              <h2>Main Name</h2>
-              <div>
-                <div>
-                  <span>2</span> Todos
-                </div>
-                <div>
-                  <span>33</span> TOTAL COMMITS
-                </div>
-              </div>
-            </div>
-          </div>
-          <div>
-            <button>...</button>
-            <button>Edit Profile</button>
-          </div>
-        </section>
-        <ul>
-          <li>Dashboard</li>
-          <li>Todo</li>
-          <li>Settings</li>
-        </ul>
-      </header>
-      <div className="dashboard">
-        <button>Add Action</button>
-        <ul>
-          <li>
-            <section></section>
-            <section>
-              <div>
-                <div>RECORDS</div>
-                <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+    <main className={styles.appMain}>
+      <div className={styles.pageContainer}>
+        <header className={styles.header}>
+          <section className={styles.profile}>
+            <div className={styles.profileInfo}>
+              <div
+                className={styles.profileImg}
+                style={{ backgroundImage: `url("logo512.png")` }}
+              />
+              <div clansName={styles.profileDetail}>
+                <h2 className={styles.profileName}>Jeong YunHo</h2>
+                <ul className={styles.profileStats}>
+                  <li className={styles.stat}>
+                    <span className={styles.count}>2</span> Todos
+                  </li>
+                  <li className={styles.stat}>
+                    <span className={styles.count}>33</span> TOTAL COMMITS
+                  </li>
                 </ul>
               </div>
-              <div>
-                <div>BEST RECORD</div>
-                <div>7 days</div>
-              </div>
-            </section>
-            <section>
-              <button>Commit Today!</button>
-            </section>
-          </li>
-        </ul>
+            </div>
+            <div className={styles.profileMore}>
+              <button className={`${styles.btn} ${styles.moreBtn}`}>...</button>
+              <button className={`${styles.btn} ${styles.editProfileBtn}`}>
+                Edit Profile
+              </button>
+            </div>
+          </section>
+          <ul className={styles.tabNavigator}>
+            <li className={`${styles.tablink} ${styles.active}`}>Dashboard</li>
+            <li className={styles.tablink}>Todo</li>
+            <li className={styles.tablink}>Settings</li>
+          </ul>
+        </header>
+        <div className="dashboard">
+          <button>Add Action</button>
+          <ul>
+            <li>
+              <section></section>
+              <section>
+                <div>
+                  <div>RECORDS</div>
+                  <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+                <div>
+                  <div>BEST RECORD</div>
+                  <div>7 days</div>
+                </div>
+              </section>
+              <section>
+                <button>Commit Today!</button>
+              </section>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   </div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AppTest from "./appTest";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppTest />
   </React.StrictMode>,
   document.getElementById("root")
 );
