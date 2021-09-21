53db527f9bbbe09692fa77d419f98fc6c6fb1de6
JeongYunHo <uno8941@gmail.com>
2021-09-21 21:58:47 +0900

    add signup form validation

--- src/signup.jsx
+++ src/signup.jsx
@@ -3,21 +3,22 @@ import signupCss from "./login.module.css";
 import commonCss from "./common.module.css";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
-import useForm from "./hooks/useForm";
 
 let styles = {};
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string() //
+    .email("Invaild email")
+    .required("Required!"),
+  password: Yup.string()
+    .min(8, "Too short!")
+    .max(12, "Too Long!")
+    .required("Required!"),
+});
 
 Object.assign(styles, signupCss, commonCss);
 
 const Signup = ({ userAuth }) => {
-  const [values, handleChange] = useForm({ email: "", password: "" });
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { email, password } = values;
-    const user = await userAuth.signup(email, password);
-    console.log(user);
-  };
-
   return (
     <div className={styles.bg}>
       <div className={styles.container}>
@@ -27,33 +28,47 @@ const Signup = ({ userAuth }) => {
             SIGNUP ON THE INTERNAL PLATFORM
           </div>
         </div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={SignupSchema}
+          onSubmit={async (values, { setSubmitting }) => {
+            const { email, password } = values;
+            const user = await userAuth.signup(email, password);
+            console.log(user);
+            setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field
+                className={styles.textInput}
+                type="email"
+                name="email"
+                placeholder="Email Address"
+              />
+              <ErrorMessage name="email" component="div" />
 
-        <form className={styles.loginForm} action="">
-          <input
-            className={styles.textInput}
-            name="email"
-            type="email"
-            placeholder="Email Address"
-            onChange={handleChange}
-          />
-          <input
-            className={styles.textInput}
-            name="password"
-            type="password"
-            placeholder="Password"
-            onChange={handleChange}
-          />
-          <div className={`${styles.indexText} ${styles.notification}`}>
-            <span>ALREADY HAS ACCOUNT?</span>
-            <a href="">LOGIN</a>
-          </div>
-          <button
-            className={`${styles.btn} ${styles.loginBtn}`}
-            onClick={handleSubmit}
-          >
-            SIGN UP
-          </button>
-        </form>
+              <Field
+                className={styles.textInput}
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              <ErrorMessage name="password" component="div" />
+              <div className={`${styles.indexText} ${styles.notification}`}>
+                <span>ALREADY HAS ACCOUNT?</span>
+                <a href="">LOGIN</a>
+              </div>
+              <button
+                className={`${styles.btn} ${styles.loginBtn}`}
+                type="submit"
+                disabled={isSubmitting}
+              >
+                SIGNUP
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
