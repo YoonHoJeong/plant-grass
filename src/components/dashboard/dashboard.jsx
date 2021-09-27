096bf0abdad3a9d315842e674a776b624e4dc905
JeongYunHo <uno8941@gmail.com>
2021-09-27 21:56:15 +0900

    add action popup in dashboard

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import dashboardCss from "./dashboard.module.css";
 import commonCss from "../../common.module.css";
 import TodoCard from "../todo_card";
-import { usePopup } from "../../hooks/usePopupForm";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
 let styles = {};
 
 Object.assign(styles, dashboardCss, commonCss);
 
+const actionSchema = Yup.object().shape({
+  title: Yup.string().required("Required!!"),
+});
+
+const useActionPopup = () => {
+  const [show, setShow] = useState(false);
+  return [
+    show ? (
+      <Formik
+        initialValues={{ title: "" }}
+        validationSchema={actionSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          const { title } = values;
+          console.log(title);
+          setShow(false);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <div className={`${styles.popupBG} ${!show && styles.hide}`}>
+            <Form className={styles.popupForm}>
+              <Field
+                className={styles.textInput}
+                type="text"
+                name="actionTitle"
+                placeholder="Enter the action title"
+              />
+              <ErrorMessage
+                className={styles.errorMsg}
+                name="actionTitle"
+                component="div"
+              />
+              <button
+                className={`${styles.btn} ${styles.loginBtn}`}
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Add Action
+              </button>
+            </Form>
+          </div>
+        )}
+      </Formik>
+    ) : null,
+    () => setShow(true),
+    () => setShow(false),
+  ];
+};
 const Dashboard = ({ todos }) => {
-  // const [popup, showPopup] = usePopup();
+  const [popup, showPopup] = useActionPopup();
 
   const handleClickAction = (e) => {
-    // showPopup();
+    showPopup();
   };
 
   return (
@@ -23,7 +72,7 @@ const Dashboard = ({ todos }) => {
       >
         Add Action
       </button>
-
+      {popup}
       <ul className={styles.todoCards}>
         <TodoCard />
       </ul>
