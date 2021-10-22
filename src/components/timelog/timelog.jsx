82c3915cdf9bf9592ccdc51e72f3a8a5d3b112aa
JeongYunHo <uno8941@gmail.com>
2021-10-22 17:40:40 +0900

    add timelog form

--- src/components/timelog/timelog.jsx
+++ src/components/timelog/timelog.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Timelog = (props) => {
-  return <div>timelog</div>;
+  return (
+    <div>
+      <form action="">
+        <section>
+          <label htmlFor="timelog-stime">start time</label>
+          <input id="timelog-stime" />
+        </section>
+        <section>
+          <label htmlFor="timelog-etime">end time</label>
+          <input id="timelog-etime" />
+        </section>
+        <section>
+          <label htmlFor="timelog-title">timelog title</label>
+          <input id="timelog-title" />
+        </section>
+        <button>submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Timelog;
